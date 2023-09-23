4c3877b402a6eaf917f45b348c3d8272341412dd
Arthur Noseda <arthur.noseda@boiron.fr>
2023-09-23 16:55:26 +0200

    GUI mode.

--- src/main/java/com/ingeniouscontraptions/octopainting/Main.java
+++ src/main/java/com/ingeniouscontraptions/octopainting/Main.java
@@ -1,33 +1,55 @@
 package com.ingeniouscontraptions.octopainting;
 
+import com.ingeniouscontraptions.octopainting.ui.OctopaintingPanel;
+
+import javax.swing.*;
 import java.io.IOException;
 import java.nio.file.Paths;
 
 public class Main {
 
     public static void main(String[] args) throws IOException, ExportException {
-        if (args.length < 2 || args.length > 3) {
-            System.err.println("Usage: java -jar octopainting-registration-form-1.0-SNAPSHOT-jar-with-dependencies.jar export <tsv> <output>\n"
+        if (args.length > 3) {
+            System.err.println("Usage: java -jar octopainting-registration-form-1.0-SNAPSHOT-jar-with-dependencies.jar gui\n"
+                    + "To display a graphical user interface.\n\n"
+                    + "Or: java -jar octopainting-registration-form-1.0-SNAPSHOT-jar-with-dependencies.jar export <tsv> <output>\n"
+                    + "To export registrations to PDF, with:\n"
                     + " - <tsv>    the path to the registrations file (a TSV file downloaded from https://framaforms.org/)\n"
-                    + " - <output> the path to the generated registration PDF file\n"
+                    + " - <output> the path to the generated registration PDF file\n\n"
                     + "Or: java -jar octopainting-registration-form-1.0-SNAPSHOT-jar-with-dependencies.jar blank <output>\n"
-                    + " - <output> the path to the blank registration PDF file\n");
+                    + "To export a blank registration to PDF, with:\n"
+                    + " - <output> the path to the blank registration PDF file");
             System.exit(1);
         }
-        // String tsv = "/home/arthur/Documents/Development/Workspaces/Java/octopainting-form-registration/src/test/resources/formulaire_dinscription_a_octopainting_2023.tsv";
-        // String output = "/home/arthur/Documents/Development/Workspaces/Java/octopainting-form-registration/output2.pdf";
-        // String output = "/home/arthur/Documents/Development/Workspaces/Java/octopainting-form-registration/blank2023.pdf";
-        Command.valueOf(args[0].toUpperCase()).execute(args);
+        if (args.length == 0) {
+            Command.GUI.execute(new String[0]);
+        } else {
+            String[] newArgs = new String[args.length - 1];
+            System.arraycopy(args, 1, newArgs, 0, newArgs.length);
+            Command.valueOf(args[0].toUpperCase()).execute(newArgs);
+        }
     }
 
     enum Command {
 
-        EXPORT {
+        GUI {
 
             @Override
             void execute(String[] args) throws IOException, ExportException {
-                String tsv = args[1];
-                String output = args[2];
+                JFrame frame = new JFrame("Octopainting Form Registration");
+                frame.getContentPane().add(new OctopaintingPanel(new Octopainting()));
+                frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+                frame.pack();
+                frame.setLocationRelativeTo(null);
+                frame.setVisible(true);
+            }
+
+        }, EXPORT {
+
+            @Override
+            void execute(String[] args) throws IOException, ExportException {
+                String tsv = args[0];
+                String output = args[1];
                 new Octopainting().exportRegistrations(Paths.get(tsv), Paths.get(output));
             }
 
@@ -35,7 +57,7 @@ public class Main {
 
             @Override
             void execute(String[] args) throws IOException, ExportException {
-                String output = args[1];
+                String output = args[0];
                 new Octopainting().exportBlank(Paths.get(output));
             }
 
